3d4302d44325775a2d25c9f350e7452099c514fd
Jianw22 <>
2022-06-06 16:13:49 +0800

    New app

--- app/webgl-video-filter.js
+++ app/webgl-video-filter.js
@@ -1,222 +1,177 @@
- class WebglVideoFilter {
+class WebglVideoFilter {
     constructor(canvas) {
-        this.canvas = canvas;
-        this.gl = canvas.getContext('webgl2');
-        this.canvasWidth = 0;
-        this.canvasHeight = 0;
-        this.initalized = false;
-        this.program = null;
-        this.textureY = null;
-        this.textureUV = null;
-        this.effectPixelBuffer = null;
+        this._canvas = canvas;
+        this._gl = canvas.getContext('webgl2');
+        this._program = null;
+
+        this._angle = 10
+        this._w = -1
+        this._h = -1
     }
-    
+
     init() {
-        if (!this.initalized) {
-            this._initProgram();
-            this._initVertexBuffers();
-            this._initTexture();
-            this.initalized = true;
-        }
+        ;
     }
 
     processVideoFrame(videoFrame) {
-        const gl = this.gl;
-        const width = videoFrame.width;
-        const height = videoFrame.height;
-        this._setSize(width, height);
-        gl.viewport(0, 0, this.canvasWidth, this.canvasHeight);
-        gl.clearColor(0, 0, 0, 0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
+        if (++this._angle >= 80) this._angle = 10;
+        const scale = Math.sin(this._angle / 180.0 * Math.PI);
 
- 
-        const uOffset = width * height;
-        gl.bindTexture(gl.TEXTURE_2D, this.textureY);
+        const size = videoFrame.height * videoFrame.width;
+
+        let planeY = {
+            data: videoFrame.data.subarray(0, size),
+            width: videoFrame.width,
+            height: videoFrame.height,
+        };
+
+        let planeUV = {
+            data: videoFrame.data.subarray(size, size * 3 / 2),
+            width: videoFrame.width,
+            height: videoFrame.height / 2,
+        };
+
+        let plane = planeY;
+        this._prepare(plane.width, plane.height);
+        // op_scale(plane.data, scale, plane.data);
+        this._op_scale_gl(this._gl, plane.data, scale, plane.data);
+    }
+
+    _op_scale_gl(gl, srcBytes, scale, dstBytes) {
+        const positionLoc = gl.getAttribLocation(_program, 'position');
+        const srcTexLoc = gl.getUniformLocation(_program, 'srcTex');
+        const srcDimensionsLoc = gl.getUniformLocation(_program, 'srcDimensions');
+        const u_scale = gl.getUniformLocation(_program, 'u_scale');
+
+        // setup a full canvas clip space quad
+        const buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+            -1, -1,
+            1, -1,
+            -1, 1,
+            -1, 1,
+            1, -1,
+            1, 1,
+        ]), gl.STATIC_DRAW);
+
+        // setup our attributes to tell WebGL how to pull
+        // the data from the buffer above to the position attribute
+        gl.enableVertexAttribArray(positionLoc);
+        gl.vertexAttribPointer(
+            positionLoc,
+            2,
+            gl.FLOAT,
+            false,
+            0,
+            0
+        );
+
+        // create our source texture
+        const srcWidth = _w;
+        const srcHeight = _h;
+        const tex = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, tex);
+        gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1); // see https://webglfundamentals.org/webgl/lessons/webgl-data-textures.html
         gl.texImage2D(
             gl.TEXTURE_2D,
             0,
             gl.LUMINANCE,
-            width,
-            height,
+            srcWidth,
+            srcHeight,
             0,
             gl.LUMINANCE,
             gl.UNSIGNED_BYTE,
-            videoFrame.data.subarray(0, uOffset)
-        );
+            new Uint8Array(srcBytes));
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
-        gl.bindTexture(gl.TEXTURE_2D, this.textureUV);
-        gl.texImage2D(
-            gl.TEXTURE_2D,
-            0,
-            gl.LUMINANCE_ALPHA,
-            width >> 1,
-            height >> 1,
-            0,
-            gl.LUMINANCE_ALPHA,
-            gl.UNSIGNED_BYTE,
-            videoFrame.data.subarray(uOffset, videoFrame.data.length)
-        );
+        //gl.useProgram(program);
+        gl.uniform1i(srcTexLoc, 0); // tell the shader the src texture is on texture unit 0
+        gl.uniform2f(srcDimensionsLoc, srcWidth, srcHeight);
+        gl.uniform1f(u_scale, scale);
 
-        gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
-        gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, this.effectPixelBuffer);
+        gl.drawArrays(gl.TRIANGLES, 0, 6); // draw 2 triangles (6 vertices)
 
-        // Get the YUV data from the effectPixelBuffer  
-        for (let i = 0; i < uOffset; i += 1) {
-            videoFrame.data[i] = this.effectPixelBuffer[4 * i];
-        }
+        // get the result
+        const dstWidth = _w;
+        const dstHeight = _h;
+        const results = new Uint8Array(dstWidth * dstHeight * 4);
+        gl.readPixels(0, 0, dstWidth, dstHeight, gl.RGBA, gl.UNSIGNED_BYTE, results);
+        // await readPixelsAsync(gl, 0, 0, dstWidth, dstHeight, gl.RGBA, gl.UNSIGNED_BYTE, results);
 
-        let widthIndex = 0;
-        let curIndex = 0;
-        for (let i = uOffset; i < videoFrame.data.length; i += 2) {
-            videoFrame.data[i] = this.effectPixelBuffer[ 4 * curIndex + 1];
-            videoFrame.data[i + 1] = this.effectPixelBuffer[4 * curIndex + 2];
-            widthIndex += 2
-            curIndex += 2
-            if (widthIndex > videoFrame.width) {
-                curIndex += videoFrame.width;
-                widthIndex = widthIndex % videoFrame.width;
-            }
+        // write(dstWidth, dstHeight, results, dstBytes);
+        for (let i = 0; i < dstWidth * dstHeight; ++i) {
+            // console.log(results[i * 4]);
+            dstBytes[i] = results[i * 4];
         }
     }
 
-    _setSize(width, height) {
-        if (this.canvasWidth !== width || this.canvasHeight !== height) {
-            this.canvas.width = width;
-            this.canvas.height = height;
-            this.canvasWidth = width;
-            this.canvasHeight = height;
-            this.effectPixelBuffer = new Uint8Array(width * height * 4)
+    _prepare(w, h) {
+        if (this._w == w && this._h == h) {
+            return 0;
         }
+
+        const vs = `
+            attribute vec4 position;
+            void main() {
+            gl_Position = position;
+            }
+        `;
+
+        const fs = `
+            precision highp float;
+
+            uniform sampler2D srcTex;
+            uniform vec2 srcDimensions;
+            uniform float u_scale;
+
+            void main() {
+            vec2 texcoord = gl_FragCoord.xy / srcDimensions;
+            vec4 value = texture2D(srcTex, texcoord);
+            gl_FragColor = value * u_scale;
+            }
+        `;
+
+        this._canvas.width = w;
+        this._canvas.height = h;
+
+        let gl = this._gl;
+        let program = this._createProgramFromSources(gl, vs, fs);
+        gl.useProgram(program);
+
+        this._program = program
+        this._w = w;
+        this._h = h;
     }
 
-    _initProgram() {
-        const vertexShaderSource = `
-            attribute vec4 a_vertexPosition;
-            attribute vec2 a_texturePosition;
-            varying vec2 v_texCoord;
-            void main() {
-                gl_Position = a_vertexPosition;
-                v_texCoord = a_texturePosition;
-            }
-        `;
-        const fragmentShaderSource = `
-            precision mediump float; 
-            varying vec2      v_texCoord; 
-            uniform sampler2D u_samplerY; 
-            uniform sampler2D u_samplerUV; 
+    _createShader(gl, type, source) {
+        var shader = gl.createShader(type);
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
+        var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+        if (success) {
+            return shader;
+        }
 
-            vec3 yuv2r = vec3(1.164, 0.0, 1.596);
-            vec3 yuv2g = vec3(1.164, -0.391, -0.813);
-            vec3 yuv2b = vec3(1.164, 2.018, 0.0);
+        console.log(gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+    }
 
-            vec3 nv12_to_rgb(vec2 texCoord) {
-                vec3 yuv; 
-                yuv.x = texture2D(u_samplerY, texCoord).r - 0.0625;
-                yuv.y = texture2D(u_samplerUV, texCoord).r - 0.5;
-                yuv.z = texture2D(u_samplerUV, texCoord).a - 0.5;
-                vec3 rgb = vec3(dot(yuv, yuv2r), dot(yuv, yuv2g), dot(yuv, yuv2b));
-                return rgb; 
-            }
-
-            vec4 rgba_to_nv12(vec3 rgb) {
-                float y = (0.257 * rgb.r) + (0.504 * rgb.g) + (0.098 * rgb.b) + 0.0625;
-                float u = -(0.148 * rgb.r) - (0.291 * rgb.g) + (0.439 * rgb.b) + 0.5;
-                float v = (0.439 * rgb.r) - (0.368 * rgb.g) - (0.071 * rgb.b) + 0.5;
-                return vec4(y, u, v, 1.0);
-            }
-
-            void main() {
-                gl_FragColor = vec4(nv12_to_rgb(v_texCoord), 1); 
-                // gray effect
-                float luminance = 0.299 * gl_FragColor.r + 0.587 * gl_FragColor.g + 0.114 * gl_FragColor.b;
-                gl_FragColor = vec4(luminance, luminance, luminance, 5);
-
-                // rgba to nv12
-                gl_FragColor = rgba_to_nv12(gl_FragColor.rgb);
-            }
-        `;
-        
-        const gl = this.gl;
-        const vertexShader = this._compileShader(vertexShaderSource, gl.VERTEX_SHADER);
-        const fragmentShader = this._compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER);
-
-        const program = gl.createProgram();
+    _createProgramFromSources(gl, vs_src, fs_src) {
+        var vertexShader = this._createShader(gl, gl.VERTEX_SHADER, vs_src);
+        var fragmentShader = this._createShader(gl, gl.FRAGMENT_SHADER, fs_src);
+        var program = gl.createProgram();
         gl.attachShader(program, vertexShader);
         gl.attachShader(program, fragmentShader);
         gl.linkProgram(program);
-        gl.useProgram(program);
-        const success = gl.getProgramParameter(program, gl.LINK_STATUS);
-        if (!success) {
-            console.error('program fail to link' + gl.getShaderInfoLog(program));
-            return;
+        var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+        if (success) {
+            return program;
         }
-        this.program = program;
-    }
-
-    _initVertexBuffers() {
-        const vertices = new Float32Array([
-            -1, -1, 0, 0.0,  0.0,
-            1, -1, 0, 1.0,  0.0,
-            -1, 1, 0, 0.0,  1.0,
-            1, 1, 0, 1.0,  1.0,
-         ]);
-
-        const gl = this.gl;
-        const program = this.program;
-        const verticeBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, verticeBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-        const positionLocation = gl.getAttribLocation(program, "a_vertexPosition");
-        gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 5 * 4, 0);
-        gl.enableVertexAttribArray(positionLocation);
-
-        const indices = new Int16Array([
-            0, 1, 2, 
-            2, 1, 3
-        ]);
-        const indicesBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-        const texcoordLocation = gl.getAttribLocation(program, "a_texturePosition");
-        gl.vertexAttribPointer(texcoordLocation, 2, gl.FLOAT, false, 5 * 4, 3 * 4);
-        gl.enableVertexAttribArray(texcoordLocation);
-    }
-
-    _createTexture() {
-        const gl = this.gl;
-        const texture = gl.createTexture();
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-       
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        return texture;
-    }
-
-    _initTexture() {
-        const gl = this.gl;
-        const program = this.program;
-        this.textureY = this._createTexture();
-        gl.uniform1i(gl.getUniformLocation(program, 'u_textureY'), 0);
-
-        gl.activeTexture(gl.TEXTURE1);
-        this.textureUV = this._createTexture();
-        gl.uniform1i(gl.getUniformLocation(program, 'u_textureUV'), 1);
-    }
-
-    _compileShader(shaderSource, shaderType) {
-        const gl = this.gl;
-        const shader = gl.createShader(shaderType);
-        gl.shaderSource(shader, shaderSource);
-        gl.compileShader(shader);
-        const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-        if (!success) {
-            const err = gl.getShaderInfoLog(shader);
-            gl.deleteShader(shader);
-            console.error('could not compile shader', err);
-            return;
-        }
-        return shader;
+        console.log(gl.getProgramInfoLog(program));
+        gl.deleteProgram(program);
     }
 }
